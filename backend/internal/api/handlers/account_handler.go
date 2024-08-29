4b9725dec6e5929b99c7069045b429b86a606ad7
Zoe Hay <zoemhay@gmail.com>
2024-08-29 16:45:04 -0700

    create account test, begin start session test

--- backend/internal/api/handlers/account_handler.go
+++ backend/internal/api/handlers/account_handler.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -10,6 +12,7 @@ import (
 	dbmodels "github.com/zoehay/gw2armoury/backend/internal/db/models"
 	"github.com/zoehay/gw2armoury/backend/internal/db/repositories"
 	"github.com/zoehay/gw2armoury/backend/internal/services"
+	"gorm.io/gorm"
 )
 
 type AccountHandler struct {
@@ -24,6 +27,66 @@ func NewAccountHandler(accountRepository repositories.AccountRepositoryInterface
 		SessionRepository: sessionRepository,
 		AccountService:    accountService,
 	}
+}
+
+func (handler AccountHandler) StartSession(c *gin.Context) {
+	var accountCreate AccountGuestCreate
+
+	if err := c.BindJSON(&accountCreate); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"request body bind json error": err.Error()})
+		return
+	}
+
+	gw2AccountID, err := handler.AccountService.GetAccountID(accountCreate.APIKey)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error could not get account id from gw2 api": err.Error()})
+		return
+	}
+
+	// var stringAccountID string
+	// if gw2AccountID != nil {
+	// 	stringAccountID = *gw2AccountID
+	// }
+
+	var account *dbmodels.DBAccount
+
+	existingAccount, err := handler.AccountRepository.GetByID(*gw2AccountID)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		account, err = handler.createAccount(*gw2AccountID, accountCreate.APIKey)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"account repository create error": err.Error()})
+			return
+		}
+	} else if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error accessing account database": err.Error()})
+		return
+	} else {
+		account = existingAccount
+	}
+
+	err = handler.generateSession(c, account)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, account)
+
+}
+
+func (handler AccountHandler) createAccount(accountID string, apiKey string) (*dbmodels.DBAccount, error) {
+
+	var newAccount = &dbmodels.DBAccount{
+		AccountID: accountID,
+		APIKey:    &apiKey,
+	}
+
+	account, err := handler.AccountRepository.Create(newAccount)
+	if err != nil {
+		return nil, err
+	}
+
+	return account, nil
 }
 
 func (handler AccountHandler) Create(c *gin.Context) {
@@ -51,15 +114,12 @@ func (handler AccountHandler) Create(c *gin.Context) {
 		stringAccountID = *gw2AccountID
 	}
 
-	// newSession := &dbmodels.DBSession{
-	// 	SessionID: '',
-	// 	Expires:   time.Now(),
-	// }
+	//password encryption
 
 	var newAccount = &dbmodels.DBAccount{
-		AccountID: stringAccountID,
-		// AccountName: &accountCreate.AccountName,
-		APIKey: &accountCreate.APIKey,
+		AccountID:   stringAccountID,
+		AccountName: &accountCreate.AccountName,
+		APIKey:      &accountCreate.APIKey,
 		// Session:     newSession,
 	}
 
@@ -95,7 +155,7 @@ func (handler AccountHandler) Login(c *gin.Context) {
 	}
 
 	// Add password verification
-	err = handler.startSession(c, account)
+	err = handler.generateSession(c, account)
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -128,23 +188,24 @@ func (handler AccountHandler) Logout(c *gin.Context) {
 	c.SetCookie("sessionID", "", -1, "/", "localhost", false, true)
 }
 
-func (handler AccountHandler) startSession(c *gin.Context, account *dbmodels.DBAccount) error {
+func (handler AccountHandler) generateSession(c *gin.Context, account *dbmodels.DBAccount) error {
 	// Create a session
 	newSessionID := generateSessionID()
 	var newSession = &dbmodels.DBSession{
 		SessionID: newSessionID,
-		Expires:   time.Now(),
+		Expires:   time.Now().Add(120 * time.Second),
 	}
 
 	session, err := handler.SessionRepository.Create(newSession)
 	if err != nil {
-		return err
+		return fmt.Errorf("startSession error creating db session: %s", err)
+
 	}
 
 	// Add session to account
 	_, err = handler.AccountRepository.UpdateSession(account.AccountID, session.SessionID)
 	if err != nil {
-		return err
+		return fmt.Errorf("startSession error updating account session: %s", err)
 	}
 
 	c.SetCookie("sessionID", session.SessionID, 3600, "/", "localhost", false, true)
@@ -163,6 +224,11 @@ type AccountLogin struct {
 }
 
 type AccountCreate struct {
-	// AccountName string
+	AccountName string
+	APIKey      string
+	Password    string
+}
+
+type AccountGuestCreate struct {
 	APIKey string
 }
--- backend/tests/start_session_test.go
+++ backend/tests/start_session_test.go
@@ -19,18 +19,18 @@ import (
 	"github.com/zoehay/gw2armoury/backend/internal/services"
 )
 
-type CreateAccountTestSuite struct {
+type StartSessionTestSuite struct {
 	suite.Suite
 	Router     *gin.Engine
 	Repository *repositories.Repository
 	Service    *services.Service
 }
 
-func TestCreateAccountTestSuite(t *testing.T) {
-	suite.Run(t, new(CreateAccountTestSuite))
+func TestStartSessionTestSuite(t *testing.T) {
+	suite.Run(t, new(StartSessionTestSuite))
 }
 
-func (s *CreateAccountTestSuite) SetupSuite() {
+func (s *StartSessionTestSuite) SetupSuite() {
 	envPath := filepath.Join("..", ".env")
 	err := godotenv.Load(envPath)
 	if err != nil {
@@ -48,18 +48,31 @@ func (s *CreateAccountTestSuite) SetupSuite() {
 	s.Service = service
 }
 
-func (s *CreateAccountTestSuite) TearDownSuite() {
-	err := s.Repository.AccountRepository.DB.Exec("DROP TABLE db_accounts cascade;").Error
-	assert.NoError(s.T(), err, "Failed to clear database")
+// func (s *StartSessionTestSuite) TearDownSuite() {
+// 	err := s.ItemService.ItemRepository.DB.Exec("DROP TABLE db_accounts;").Error
+// 	assert.NoError(s.T(), err, "Failed to clear database")
 
-	db, err := s.Repository.AccountRepository.DB.DB()
-	if err != nil {
-		s.T().Fatal(err)
-	}
-	db.Close()
-}
+// 	db, err := s.ItemService.ItemRepository.DB.DB()
+// 	if err != nil {
+// 		s.T().Fatal(err)
+// 	}
+// 	db.Close()
+// }
 
-func (s *CreateAccountTestSuite) TestCreateAccount() {
+func (s *StartSessionTestSuite) TestStartSession() {
+	// w := httptest.NewRecorder()
+	// c, _ := gin.CreateTestContext(w)
+	// req := &http.Request{
+	// 	URL:    &url.URL{},
+	// 	Header: make(http.Header),
+	// }
+
+	// q := req.URL.Query()
+	// q.Add("id", "27952")
+	// req.URL.RawQuery = q.Encode()
+
+	// c.Request = req
+
 	userJson := `{"AccountName":"Name forAccount", "APIKey":"stringthatisapikey", "Password":"stringthatispassword"}`
 
 	w := httptest.NewRecorder()
@@ -68,14 +81,4 @@ func (s *CreateAccountTestSuite) TestCreateAccount() {
 
 	fmt.Println(w.Body.String())
 	assert.Equal(s.T(), 200, w.Code)
-}
-
-func (s *CreateAccountTestSuite) TestFailCreateAccount() {
-	userJson := `{"AccountName":"Name forAccount", "APIKey":"stringthatisapikey"}`
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/signup", strings.NewReader(userJson))
-	s.Router.ServeHTTP(w, req)
-
-	assert.Equal(s.T(), 500, w.Code)
 }
